cbcc54795a92cf19148bbd9e0b650f16ce2fb682
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-05 11:49:20 +0900

    삼항 연산자/if

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++){
+        String color = "보라";
 
-            if(i % 5 == 0){
-                continue;
-            }
+        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : (color == "초록") ? "초록" : "보라";
 
-            if(i >= 15){
-                break;
-            }
-            
-            System.out.println(i);
-        }
+        // if문 변환
+        /*if(color == "빨강"){
+            choiceColor = "빨강";
+        } else if (color == "파랑") {
+            choiceColor = "파랑";
+        } else if (color == "초록") {
+            choiceColor = "초록";
+        } else{
+            choiceColor = "보라";
+        }*/
+
+        System.out.println(choiceColor);
     }
 }
