72ef59f0c39083bf86e5ccc0b58d6e0abf74ae57
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-05 15:41:05 +0900

    함수

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
 public class Main {
+    public static int addNumber(int a, int b){
+        return a + b;
+    }
+
     public static void main(String[] args) {
-        String color = "보라";
-
-        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : (color == "초록") ? "초록" : "보라";
-
-        // if문 변환
-        /*if(color == "빨강"){
-            choiceColor = "빨강";
-        } else if (color == "파랑") {
-            choiceColor = "파랑";
-        } else if (color == "초록") {
-            choiceColor = "초록";
-        } else{
-            choiceColor = "보라";
-        }*/
-
-        System.out.println(choiceColor);
+        int number = addNumber(10, 7);
+        System.out.println(number);
     }
 }
